5d1c759d8090882aed63d3e4ff9ce9be759f0b79
Kagetori <Kagetori@users.noreply.github.com>
2025-03-18 00:48:18 -0600

    Add number validation

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function calculate() {
-    let numPeople = document.querySelector("#num-people").value;
-    // document.getElementById("display").innerHTML = numPeople;
-
+    let numPeople = document.getElementById("num-people").value;
+    
     // validate input
-    isValidNum(numPeople);
+    let isValid = isValidNum(Number(numPeople));
+    if (!isValid) {
+        return;
+    }
+
+    document.getElementById("display").innerHTML = numPeople;
 }
 
 function isValidNum(numPeople) {
-    // let isValid = Number.isInteger(numPeople) && numPeople > 0 && numPeople <= 100;
-
-    if 
+    let isValid = Number.isInteger(numPeople) && numPeople > 0 && numPeople < 100;
+    console.log(Number.isInteger(numPeople));
+    document.getElementById("validation").style.display = isValid ? "none" : "block";
+    return isValid;
 }
