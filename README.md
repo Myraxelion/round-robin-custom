fd48931f4000d2bf84e897300dc418837a022361
Kagetori <Kagetori@users.noreply.github.com>
2025-03-18 15:26:07 -0600

    Add func to initialize players

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # round-robin-custom
+Welcome! Input the number of people and courts, and the generator will tell you who is playing who for each round. Add or minus people at any time!
--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-function calculate() {
-    let numPeople = document.getElementById("num-people").value;
+var currentRound = 0;
+var players = [];
+
+function start() {
+    let numPlayers = Number(document.getElementById("num-people").value);
     
     // validate input
-    let isValid = isValidNum(Number(numPeople));
+    let isValid = isValidNum(numPlayers);
     if (!isValid) {
         return;
     }
 
-    document.getElementById("display").innerHTML = numPeople;
+    // hide initial input form
+    // document.getElementById("input").style.display = "none";
+
+    initializePlayers(numPlayers);
+
+    // go to next (first) round
+    nextRound();
+
+    document.getElementById("display").innerHTML = numPlayers;
+    console.log(players);
 }
 
-function isValidNum(numPeople) {
-    let isValid = Number.isInteger(numPeople) && numPeople > 0 && numPeople < 100;
-    console.log(Number.isInteger(numPeople));
+function isValidNum(num) {
+    let isValid = Number.isInteger(num) && num > 0 && num < 100;
     document.getElementById("validation").style.display = isValid ? "none" : "block";
+
     return isValid;
+}
+
+function initializePlayers(numPlayers) {
+    for (let i = 0; i < numPlayers; i++) {
+        players.push({
+            id: i+1,
+            playCount: 0
+        })
+    }
+}
+
+function nextRound() {
+    // TODO
+    return;
 }
